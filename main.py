9ea824347b97ab3e9a9abc18f92800f27425809b
Siddartha Kommu <kommusiddartha001@gmail.com>
2025-03-21 21:17:27 +0530

    Made some changes

--- main.py
+++ main.py
@@ -1,55 +1,60 @@
 from data_cleaning import clean_and_transform_data
 from fake_data_generation import generate_fake_data
 import pandas as pd
-#from data_cleaning import load_data_to_bigquery
+from data_cleaning import load_data_to_bigquery
+from config import PROJECT_ID,DATASET_ID
+from google.cloud import bigquery
+#from kpi import execute_kpi_queries
+from kpi2 import kpi_queries
+
 
 
 def main():
 
-    # Download and load dataset
-    #download_kaggle_dataset('olistbr/brazilian-ecommerce', path='./data')
+#     # Download and load dataset
+#     #download_kaggle_dataset('olistbr/brazilian-ecommerce', path='./data')
 
-    #Load individual DataFrames
-    customers = pd.read_csv(r"data/olist_customers_dataset.csv")
-    geolocation = pd.read_csv(r"data/olist_geolocation_dataset.csv")
-    order_items = pd.read_csv(r"data/olist_order_items_dataset.csv")
-    order_payments = pd.read_csv(r"data/olist_order_payments_dataset.csv")
-    order_reviews = pd.read_csv(r"data/olist_order_reviews_dataset.csv")
-    orders = pd.read_csv(r"data/olist_orders_dataset.csv")
-    products = pd.read_csv(r"data/olist_products_dataset.csv")
-    sellers = pd.read_csv(r"data/olist_sellers_dataset.csv")
-    category_translation = pd.read_csv(r"data/product_category_name_translation.csv")
+#     #Load individual DataFrames
+#     customers = pd.read_csv(r"data/olist_customers_dataset.csv")
+#     geolocation = pd.read_csv(r"data/olist_geolocation_dataset.csv")
+#     order_items = pd.read_csv(r"data/olist_order_items_dataset.csv")
+#     order_payments = pd.read_csv(r"data/olist_order_payments_dataset.csv")
+#     order_reviews = pd.read_csv(r"data/olist_order_reviews_dataset.csv") 
+#     orders = pd.read_csv(r"data/olist_orders_dataset.csv")
+#     products = pd.read_csv(r"data/olist_products_dataset.csv")
+#     sellers = pd.read_csv(r"data/olist_sellers_dataset.csv")
+#     category_translation = pd.read_csv(r"data/product_category_name_translation.csv")
 
-    # Merge DataFrames
-    df = orders.merge(customers, on="customer_id", how="left")
-    df = df.merge(order_payments, on="order_id", how="left")
-    df = df.merge(order_reviews, on="order_id", how="left")
-    df = df.merge(order_items, on="order_id", how="left")
-    df = df.merge(products, on="product_id", how="left")
-    df = df.merge(category_translation, on="product_category_name", how="left")
-    df = df.merge(sellers, on="seller_id", how="left")
+#     # Merge DataFrames
+#     df = orders.merge(customers, on="customer_id", how="left")
+#     df = df.merge(order_payments, on="order_id", how="left")
+#     df = df.merge(order_reviews, on="order_id", how="left")
+#     df = df.merge(order_items, on="order_id", how="left")
+#     df = df.merge(products, on="product_id", how="left")
+#     df = df.merge(category_translation, on="product_category_name", how="left")
+#     df = df.merge(sellers, on="seller_id", how="left")
 
-    #Clean and transform data  gi
-    df = clean_and_transform_data(df)
-    df.to_csv('firstframe.csv',index=False)
+#     #Clean and transform data  gi
+#     df = clean_and_transform_data(df)
+#     df.to_csv('firstframe.csv',index=False)
 
-    #Generating Fake Data
-    fake_df = generate_fake_data(20000)
-    fake_df.to_csv('secondframe.csv',index=False)
+#     #Generating Fake Data
+#     fake_df = generate_fake_data(20000)
+#     fake_df.to_csv('secondframe.csv',index=False)
 
-    # Append fake data to original data
-    df_merged = pd.concat([df, fake_df], ignore_index=True)
-    # Save merged DataFrame to CSV
-    df_merged.to_csv('dataframe.csv', index=False)
+#     # Append fake data to original data
+#     df_merged = pd.concat([df, fake_df], ignore_index=True)
+#     # Save merged DataFrame to CSV
+#     df_merged.to_csv('dataframe.csv', index=False)
 
 
-    # def load_data_to_bigquery(df, table_name):
-    #  """Loads the cleaned data into BigQuery."""
-    # table_id = f"{PROJECT_ID}.{DATASET_ID}.{table_name}"
-    # job_config = bigquery.LoadJobConfig(write_disposition="WRITE_TRUNCATE")
-    # job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
-    # job.result()  # Wait for the job to complete
-    # print(f"Data loaded to {table_name} successfully.")
+#     def load_data_to_bigquery(df, table_name):
+#         client = bigquery.Client(project=PROJECT_ID)
+#         table_id = f"{PROJECT_ID}.{DATASET_ID}.{table_name}"
+#         job_config = bigquery.LoadJobConfig(write_disposition="WRITE_TRUNCATE")
+#         job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
+#         job.result()  # Wait for the job to complete
+#         print(f"Data loaded to {table_name} successfully.")
 
 
 #     fact_orders = df[[
@@ -81,13 +86,16 @@ def main():
 #     dim_payment_types = df[["payment_type"]].drop_duplicates().reset_index(drop=True)  # Reset index
 #     dim_payment_types["payment_type_id"] = dim_payment_types.index  # Assign numeric IDs
 
-    # Load data into BigQuery
-    # load_data_to_bigquery(fact_orders, 'fact_orders')
-    # load_data_to_bigquery(dim_customers, 'dim_customers')
-    # load_data_to_bigquery(dim_products, 'dim_products')
-    # load_data_to_bigquery(dim_orders, 'dim_orders')
-    #load_data_to_bigquery(dim_sellers, 'dim_sellers')
-    #load_data_to_bigquery(dim_payment_types, 'dim_payment_types')
+#     #Load data into BigQuery
+#     load_data_to_bigquery(fact_orders, 'fact_orders')
+#     load_data_to_bigquery(dim_customers, 'dim_customers')
+#     load_data_to_bigquery(dim_products, 'dim_products')
+#     load_data_to_bigquery(dim_orders, 'dim_orders')
+#     load_data_to_bigquery(dim_sellers, 'dim_sellers')
+#     load_data_to_bigquery(dim_payment_types, 'dim_payment_types')
+
+    #execute_kpi_queries()
+    kpi_queries()
    
 
 if __name__ == "__main__":
